d7c40db24b3bd2200a833bfa63cf0a3d5c8fc800
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:07:32 +0500

    get-3

--- get_user_country.py
+++ get_user_country.py
@@ -8,4 +8,19 @@ def get_user_country(data:list, name:str) -> list:
     Returns:
         str: The country of the user with the given name
     """
-    return 
+    max_age_name=[]
+    for user in data:
+        if user['country']==name:
+            max_age_name.append(user['country'])
+
+    return max_age_name
+print(get_user_country(data=[
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  }
+], name="John")) 
