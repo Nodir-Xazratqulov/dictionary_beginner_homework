799251e6041c7a1e6b1bb409c8dbfde9421e9d49
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:13:14 +0500

    sum-3

--- sum_even_values.py
+++ sum_even_values.py
@@ -6,4 +6,19 @@ def sum_even_values(data: dict) -> int:
     Returns:
         int: The sum of all even values in the dictionary
     '''
-    return 
+    list1=[]
+    count=0
+    for i in data:
+        if i:
+            list1.append(data[i])
+        
+    for j in list1:
+        if j%2==0:
+            count+=j   
+            
+    return count
+print(sum_even_values(data={
+    'a': 1, 
+    'b': 2, 
+    'c': 3
+}))
