20c221646317869bca814c603dfcdba85c3cd2a6
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:06:18 +0500

    max-2

--- get_min_age_user_name.py
+++ get_min_age_user_name.py
@@ -7,4 +7,20 @@ def get_min_age_user_name(data:list) -> str:
     Returns:
         str: The name of the user with the minimum age in the dictionary
     """
-    return 
+    max_age=1000
+    max_age_name=''
+    for user in data:
+        if user['age']<max_age:
+            max_age=user['age']
+            max_age_name=user['name']
+    return max_age_name
+print(get_min_age_user_name(data=[
+  {
+    'name': 'John', 
+    'age': 56
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 42
+  }
+]))
