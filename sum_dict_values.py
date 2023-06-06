f10d352386683e16b4c7a05539a61eb8c468e690
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:12:25 +0500

    sum-2

--- sum_dict_values.py
+++ sum_dict_values.py
@@ -6,4 +6,18 @@ def sum_dict_values(data: dict) -> int:
     Returns:
         int: The sum of all values in the dictionary
     '''
-    return 
+    list1=[]
+    count=0
+    for i in data:
+        if i:
+            list1.append(data[i])
+        
+    for j in list1:
+            count+=j   
+            
+    return count
+print(sum_dict_values(data={
+    'a': 1, 
+    'b': 2, 
+    'c': 3
+}))
