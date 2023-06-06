26e4e86b93b45fd504418f0454a75d46bd013fda
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:14:23 +0500

    sum-4

--- sum_float_values.py
+++ sum_float_values.py
@@ -6,4 +6,26 @@ def sum_float_values(data: dict) -> float:
     Returns:
         float: The sum of all float values in the dictionary.
     '''
-    return 
+
+    list1=[]
+    count=0
+    for i in data:
+        list1.append(data[i])
+    
+        
+    for j in list1:
+        
+        # if str(j).find(".")!=-1:
+        if type(j)==float:
+            count+=j  
+            
+          
+    return count
+print(sum_float_values(data={
+    'a': 1, 
+    'b': 2.5,  
+    'c': 3.0
+}))
+
+
+#???
