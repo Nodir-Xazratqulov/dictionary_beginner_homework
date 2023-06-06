c72a5672a7225965748ce50fa365bccfaf3eb997
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:02:46 +0500

    count-2

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -8,4 +8,26 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    return 
+    count=0
+    for i in data:
+        if i['age']==27:
+            count+=1
+
+    return count
+
+
+
+print(count_users_with_age(data=[
+  {
+    'name': 'John',
+    'age': 27
+  },
+  {
+    'name':'Mary', 
+    'age': 42
+  },
+  {
+    'name':'Ann',
+    'age': 27
+  }
+  ],age=27))
