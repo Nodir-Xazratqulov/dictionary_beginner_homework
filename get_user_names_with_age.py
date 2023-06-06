fd6a295280e44e8ff52397892a97eb59aa6046d4
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-06 10:09:20 +0500

    get-5

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,4 +8,19 @@ def get_user_names_with_age(data:list, age:int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    return 
+    user_name=[]
+    for user in data:
+        if user['age']==age:
+            age=user['age']
+            user_name.append(user['name'])
+    return user_name
+print(get_user_names_with_age(data=[
+  {
+    'name': 'John', 
+    'age': 27
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 42
+  }
+], age=27))
